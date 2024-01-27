5b6f9b1588d4024f462665e2711579b194eafed9
Rubenbaral <Rubenbaral3@gmail.com>
2024-01-27 00:21:23 -0600

    Updated routing for ProductDetails

--- ecommerce-platform/frontend/src/App.js
+++ ecommerce-platform/frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import ProductList from './components/ProductList';
+import ProductDetails from './components/ProductDetails';
 import Footer from './components/Footer';
 import Home from './components/Home';
 
@@ -18,6 +19,9 @@ function App() {
             </Route>
             <Route path="/products">
               <ProductList />
+            </Route>
+            <Route path="/products/:productId">
+              <ProductDetails />
             </Route>
             {/* Additional routes can be added here */}
           </Switch>
