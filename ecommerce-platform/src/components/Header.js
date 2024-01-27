1930ff9bece35b1292292e82ac6894fc7dd35204
Rubenbaral <Rubenbaral3@gmail.com>
2024-01-27 00:05:06 -0600

    Added new components to the project

---
+++ ecommerce-platform/src/components/Header.js
@@ -0,0 +1,14 @@
+// src/components/Header.js
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-blue-500 text-white p-4">
+      <h1 className="text-lg">Cloud Shopper Hub</h1>
+      {/* Navigation links or logo here */}
+    </header>
+  );
+};
+
+export default Header;
