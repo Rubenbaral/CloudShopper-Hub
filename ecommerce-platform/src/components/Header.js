5e1b0807ca6f70632a3f9d43e5554adb5ee732d4
Rubenbaral <Rubenbaral3@gmail.com>
2024-01-29 15:05:00 -0600

    Improved styling and responsiveness

--- ecommerce-platform/src/components/Header.js
+++ ecommerce-platform/src/components/Header.js
@@ -4,9 +4,15 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-blue-500 text-white p-4">
+    <header className="bg-blue-500 text-white p-4 flex justify-between">
       <h1 className="text-lg">Cloud Shopper Hub</h1>
-      {/* Navigation links or logo here */}
+      <nav>
+        <ul className="flex space-x-4">
+          <li><a href="/">Home</a></li>
+          <li><a href="/products">Products</a></li>
+          {/* Add other navigation links here */}
+        </ul>
+      </nav>
     </header>
   );
 };
